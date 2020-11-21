0a36cd2c681d99109cde3c27d630915f77287b6e
Justin Schwab <justin-schwab@web.de>
2020-11-21 17:49:48 +0100

    Tooltips zu SkillSO hinzugefügt

--- Catventure/Assets/ScriptableObjects/Skills/SkillSO.cs
+++ Catventure/Assets/ScriptableObjects/Skills/SkillSO.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 
 public class SkillSO : ScriptableObject
 {
-    public SkillTreeType skillTreeType;
+    [Tooltip("Which skilltree contains this skill.")]
+    public SkillTreeType skilltreeType;
+    [Tooltip("Level that is needed to learn this skill.")]
     public int level;
+    [Tooltip("Skills the player has to learn before he can learn this skill.")]
     public List<SkillSO> skillsNeeded;
+    [Tooltip("Number of skillpoints needed to learn this skill.")]
     public int skillPointsNeeded;
+    [Tooltip("How much the physical attack of the player gets buffed. This value adds up on other skills.")]
     public int attackBonus;
+    [Tooltip("How much the defense of the player gets buffed. This value adds up on other skills.")]
     public int defenseBonus;
+    [Tooltip("How much the magic attack of the player gets buffed. This value adds up on other skills.")]
     public int magicBonus;
 }
