3cd8b9316261761ee9e6ddcbe895778ccdd2ccbc
Justin Schwab <justin-schwab@web.de>
2020-11-21 17:42:37 +0100

    Added CharacterMovement, Added SkillSO (ScriptableObject in dem die Skills gespeichert werden)

---
+++ Catventure/Assets/ScriptableObjects/Skills/SkillSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Skill", menuName = "ScriptableObjects/Skill")]
+
+public class SkillSO : ScriptableObject
+{
+    public SkillTreeType skillTreeType;
+    public int level;
+    public List<SkillSO> skillsNeeded;
+    public int skillPointsNeeded;
+    public int attackBonus;
+    public int defenseBonus;
+    public int magicBonus;
+}
