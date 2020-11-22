e70690a424ef33e7982639faa4e4c6f0ef55c296
Justin Schwab <justin-schwab@web.de>
2020-11-22 13:42:11 +0100

    worked on Skilltree

--- Catventure/Assets/Scripts/Skilltree.cs
+++ Catventure/Assets/Scripts/Skilltree.cs
@@ -16,8 +16,10 @@ public class Skilltree : MonoBehaviour
     GameObject fireTree;
     GameObject iceTree;
     GameObject earthTree;
+    Camera cam;
     void Start()
     {
+        cam = Camera.main;
         allSkills = Resources.LoadAll<SkillSO>("ScriptableObjects/Skills").ToList();
         fireSkills = allSkills.Where(s => s.skilltreeType == SkillTreeType.Fire).ToList();
         fireSkills = fireSkills.OrderBy(s => s.level).ToList();
@@ -29,21 +31,36 @@ public class Skilltree : MonoBehaviour
         fireTree = gameObject.transform.Find("FireTree").gameObject;
         iceTree = gameObject.transform.Find("IceTree").gameObject;
         earthTree = gameObject.transform.Find("EarthTree").gameObject;
-
+        float xPosition = 5;
+        int previousLevel = 0;
         foreach (SkillSO skill in fireSkills)
         {
+            Debug.Log(skill);
             GameObject currentSkill = Instantiate(skillTreeButtonPrefab);
+            RectTransform currentTransform = currentSkill.GetComponent<RectTransform>();
             currentSkill.transform.SetParent(fireTree.transform.Find(skill.level.ToString()));
             currentSkill.name = skill.name;
             currentSkill.GetComponentInChildren<TMP_Text>().text = skill.name;
             currentSkill.transform.localScale = Vector3.one;
-            if(skill.skillNeeded)
+            currentSkill.GetComponent<RectTransform>().anchoredPosition3D = Vector3.zero;
+            if (previousLevel == skill.level)
             {
-                Debug.Log("Drawing Line");
+                xPosition += 155;
+            }
+            else
+            {
+                xPosition = 5;
+            }
+            currentSkill.GetComponent<RectTransform>().anchoredPosition = new Vector2(xPosition, currentTransform.anchoredPosition.y);
+            if (skill.skillNeeded)
+            {
                 LineRenderer lineRend = currentSkill.GetComponent<LineRenderer>();
                 lineRend.SetPosition(0, currentSkill.transform.position);
+                Debug.Log("Drawing Line from " + currentSkill.transform.position + " " + currentSkill.name);
                 lineRend.SetPosition(1, fireTree.transform.Find(skill.skillNeeded.level.ToString()).Find(skill.skillNeeded.name).position);
+                Debug.Log("Drawing Line to " + fireTree.transform.Find(skill.skillNeeded.level.ToString()).Find(skill.skillNeeded.name).position + " " + fireTree.transform.Find(skill.skillNeeded.level.ToString()).Find(skill.skillNeeded.name).gameObject.name);
             }
+            previousLevel = skill.level;
         }
     }
 
