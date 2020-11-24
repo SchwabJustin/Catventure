3686d0f45984042dc86fde41cc3095c61edec4ff
Justin Schwab <justin-schwab@web.de>
2020-11-24 18:45:27 +0100

    Added Platforms

---
+++ Catventure/Assets/Scripts/LevelElements/MovingPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1.position, pos2.position, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
---
+++ Catventure/Assets/Scripts/System/PlayerManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Homebrew;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Foldout("PlayerStats", true)] 
+    [Tooltip("Current Health of the Player")]
+    public int currentPlayerHealth = 3;
+    [Tooltip("Maximal Health of the Player")]
+    public int maxPlayerHealth = 3;
+    [Tooltip("Damage the Player deals with Attacks")]
+    public int playerAttackDmg = 1;
+    [Tooltip("Damage the Player deals with Magic")]
+    public int playerMagicDmg = 1;
+
+    [Foldout("SkillStuff", true)]
+    [Tooltip("Current Skill Points the player has")]
+    public int currentSkillPoints;
+    [Tooltip("Current Skills the Player has learned")]
+    public List<SkillSO> learnedSkills;
+
+    public void LearnSkill(SkillSO skillToLearn, Button skillBtn)
+    {
+        Debug.Log("Trying to learn Skill " + skillToLearn.name);
+        if (learnedSkills.Contains(skillToLearn))
+        {
+            Debug.Log("You already learned this skill");
+        }
+        else if (skillToLearn.skillNeeded)
+        {
+            if (!learnedSkills.Contains(skillToLearn.skillNeeded))
+            {
+                Debug.Log("You need to learn " + skillToLearn.skillNeeded.name + " to learn this skill.");
+            }
+
+            else if (skillToLearn.skillPointsNeeded > currentSkillPoints)
+            {
+                Debug.Log("You don't have enough skill points.");
+            }
+            else
+            {
+                currentSkillPoints -= skillToLearn.skillPointsNeeded;
+                learnedSkills.Add(skillToLearn);
+                if (skillBtn)
+                {
+                    skillBtn.interactable = false;
+                }
+                Debug.Log("Learned Skill " + skillToLearn.name);
+            }
+        }
+
+        else if (skillToLearn.skillPointsNeeded > currentSkillPoints)
+        {
+            Debug.Log("You don't have enough skill points.");
+        }
+        else
+        {
+            currentSkillPoints -= skillToLearn.skillPointsNeeded;
+            learnedSkills.Add(skillToLearn);
+            if (skillBtn)
+            {
+                skillBtn.interactable = false;
+            }
+            Debug.Log("Learned Skill " + skillToLearn.name);
+        }
+    }
+
+}
