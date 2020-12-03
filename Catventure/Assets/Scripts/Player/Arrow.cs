3dd04ce2565b6008827f6320b28ea343ad8cbc23
Justin Schwab <justin-schwab@web.de>
2020-12-03 01:40:25 +0100

    Added Bow and Weapon Sprites

---
+++ Catventure/Assets/Scripts/Player/Arrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool hasHit;
+    void Start()
+    {
+        BoxCollider2D playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), playerCollider);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.right);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        hasHit = true;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Catventure/Assets/Scripts/Player/Bow.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    public GameObject arrow;
+
+    public float launchForce;
+    public float maxLaunchForce;
+    public float launchForceAddUp;
+    public float cooldown;
+    public Transform shotPoint;
+
+    public GameObject markerPoint;
+    private GameObject[] points;
+    public int numberOfPoints;
+    public float spaceBetweenPoints;
+    private Vector2 direction;
+    private bool shot;
+    private bool shooting;
+
+    private void Start()
+    {
+        points = new GameObject[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i] = Instantiate(markerPoint, shotPoint.position, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        Vector2 bowPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePosition - bowPosition;
+        transform.right = direction;
+        if (Input.GetMouseButtonDown(0) && !shot)
+        {
+            shooting = true;
+            StartCoroutine(IncreaseLaunchForce());
+        }
+        if (Input.GetMouseButtonUp(0) && !shot && shooting)
+        {
+            shot = true;
+            Shoot();
+            launchForce = 1;
+        }
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+        }
+    }
+
+    private IEnumerator IncreaseLaunchForce()
+    {
+        while (!shot)
+        {
+            if (launchForce < maxLaunchForce)
+            {
+                launchForce += launchForceAddUp;
+                yield return new WaitForSeconds(0.01f);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+        newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+        StartCoroutine(AfterShot());
+    }
+
+    private IEnumerator AfterShot()
+    {
+        yield return new WaitForSeconds(cooldown);
+        shot = false;
+        shooting = false;
+    }
+
+    Vector2 PointPosition(float t)
+    {
+        Vector2 position = (Vector2) shotPoint.position + (direction.normalized * launchForce * t) +
+                           0.5f * Physics2D.gravity * (t * t);
+        return position;
+    }
+}
