dca3dfb2420129eac141c79dfbf08c62547936ca
Justin Schwab <justin-schwab@web.de>
2021-07-27 00:20:45 +0200

    added shootingEnemy

---
+++ Catventure/Assets/Scripts/LevelElements/Enemies/EnemyProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public int damage = 1;
+    public float timeTillSelfDestruct = 2;
+
+    void Start()
+    {
+        StartCoroutine(SelfDestructTimer());
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<PlayerManager>().GotDamaged(damage);
+            Destroy(this.gameObject);
+        }
+    }
+    IEnumerator SelfDestructTimer()
+    {
+        yield return new WaitForSeconds(timeTillSelfDestruct);
+        Destroy(this.gameObject);
+    }
+}
--- Catventure/Assets/Scripts/LevelElements/Enemies/ShootingEnemy.cs
+++ Catventure/Assets/Scripts/LevelElements/Enemies/ShootingEnemy.cs
@@ -19,31 +19,48 @@ public class ShootingEnemy : MonoBehaviour
     [Tooltip("Position where Projectile Spawns")]
     public GameObject shootPosition;
     [Tooltip("Time till Enemy can shoot again in Seconds")]
+    public float shootForce = 4;
+    [Tooltip("Speed of projectile")]
     public float cooldownTime;
     [Tooltip("True if Enemy can shoot")]
-    private bool cooldown;
+    public bool cooldown;
     void Update()
     {
         if (!playerSighted)
         {
             transform.position = Vector3.Lerp(pos1.position, pos2.position, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
         }
+        if (transform.position == pos1.position)
+        {
+            transform.localScale = Vector3.one;
+        }
+        if (transform.position == pos2.position)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            Shoot();
+            Shoot(col.gameObject.transform);
         }
     }
 
-    public void Shoot()
+    public void Shoot(Transform playerTransform)
     {
-        if (cooldown)
+        if (!cooldown)
         {
-            cooldown = false;
-            Instantiate(enemyProjectilePrefab, shootPosition.transform);
+            cooldown = true;
+            var projectile = Instantiate(enemyProjectilePrefab, shootPosition.transform);
+            projectile.GetComponent<Rigidbody2D>().AddForce(Vector2.MoveTowards(shootPosition.transform.position, playerTransform.position, 3) * shootForce);
+            StartCoroutine(ResetCooldown());
         }
+    }
+    IEnumerator ResetCooldown()
+    {
+        yield return new WaitForSeconds(cooldownTime);
+        cooldown = false;
     }
 }
